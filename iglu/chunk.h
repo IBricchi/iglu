bbe55255a32033e09bdcb0eabbd10ce0dcd22b66
IBricchi <ignacio.bricchi@gmail.com>
2020-08-17 15:48:08 +0200

    Add chunk struct

--- iglu/chunk.h
+++ iglu/chunk.h
@@ -1,6 +1,19 @@
 #pragma once
-class chunk
+
+#include <vector>
+
+enum class OpCode {
+	OP_RETURN,
+
+};
+
+class Chunk
 {
-	
+private:
+	std::vector<uint8_t> code;
+	std::vector<int> lines;
+public:
+	Chunk();
+	void writeChunk(uint8_t, int);
 };
 
